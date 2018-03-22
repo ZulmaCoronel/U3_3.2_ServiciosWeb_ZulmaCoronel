6ad4eab436c1dd3e9948767f37c69ab3c052fb80
Zulma <zuiscoronello@ittepic.edu.mx>
2018-03-21 23:03:44 -0700

    actualizado

--- app/src/main/java/mx/edu/ittepic/u3_servicios/MainActivity.java
+++ app/src/main/java/mx/edu/ittepic/u3_servicios/MainActivity.java
@@ -11,21 +11,39 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.view.View;
 
+import  com.google.android.gms.common.api.PendingResult ;
+import  com.google.android.gms.common.api.ResultCallback ;
+import  com.google.android.gms.common.api.Status ;
+import  com.google.android.gms.location.LocationListener ;
+import  com.google.android.gms.location.LocationRequest ;
+import  com.google.android.gms.location.LocationSettingsRequest ;
+import  com.google.android.gms.location.LocationSettingsResult ;
+import  com.google.android.gms.location.LocationSettingsStatusCodes ;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
-                                    GoogleApiClient.ConnectionCallbacks {
+
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     private static final String LOGTAG = "android-localizacion";
+
     private static final int PETICION_PERMISO_LOCALIZACION = 101;
+    private static final int PETICION_CONFIG_UBICACION = 201;
+
     private GoogleApiClient apiClient;
 
     private TextView lblLatitud;
     private TextView lblLongitud;
     private ToggleButton btnActualizar;
+
+    private LocationRequest locRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +54,104 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         lblLongitud = (TextView) findViewById(R.id.lblLongitud);
         btnActualizar = (ToggleButton) findViewById(R.id.btnActualizar);
 
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleLocationUpdates(btnActualizar.isChecked());
+            }
+        });
+
+
+    //GoogleApiClient sera el punto de acceso a los servicios de Google Play.
+        // Para la creación de este objeto se debe indicar la API a la que queremos acceder
+        // y como queremos gestionar la conexión con los servicios
         apiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, this)
                 .addConnectionCallbacks(this)
                 .addApi(LocationServices.API)
                 .build();
     }
+
+
+
+     private void toggleLocationUpdates(boolean enable) {
+        if (enable) {
+            enableLocationUpdates();
+        } else {
+            disableLocationUpdates();
+        }
+    }
+
+    private void enableLocationUpdates() {
+
+        locRequest = new LocationRequest();
+        locRequest.setInterval(2000);
+        locRequest.setFastestInterval(1000);
+        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest locSettingsRequest =
+                new LocationSettingsRequest.Builder()
+                        .addLocationRequest(locRequest)
+                        .build();
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        apiClient, locSettingsRequest);
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult locationSettingsResult) {
+                final Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+
+                        Log.i(LOGTAG, "Configuración correcta");
+                        startLocationUpdates();
+
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            Log.i(LOGTAG, "Se requiere actuación del usuario");
+                            status.startResolutionForResult(MainActivity.this, PETICION_CONFIG_UBICACION);
+                        } catch (IntentSender.SendIntentException e) {
+                            btnActualizar.setChecked(false);
+                            Log.i(LOGTAG, "Error al intentar solucionar configuración de ubicación");
+                        }
+
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i(LOGTAG, "No se puede cumplir la configuración de ubicación necesaria");
+                        btnActualizar.setChecked(false);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void disableLocationUpdates() {
+
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                apiClient, this);
+
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            //Ojo: estamos suponiendo que ya tenemos concedido el permiso.
+            //Sería recomendable implementar la posible petición en caso de no tenerlo.
+
+            Log.i(LOGTAG, "Inicio de recepción de ubicaciones");
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    apiClient, locRequest, MainActivity.this);
+        }
+    }
+
+
+
+
 
     @Override
     public void onConnectionFailed(ConnectionResult result) {
@@ -77,6 +187,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         Log.e(LOGTAG, "Se ha interrumpido la conexión con Google Play Services");
     }
 
+    //Metodo updateUI: actualiza los campos de latitud y longitud a partir de un objeto Location
     private void updateUI(Location loc) {
         if (loc != null) {
             lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
@@ -109,5 +220,40 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             }
         }
     }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PETICION_CONFIG_UBICACION:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(LOGTAG, "El usuario no ha realizado los cambios de configuración necesarios");
+                        btnActualizar.setChecked(false);
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        Log.i(LOGTAG, "Recibida nueva ubicación!");
+
+        //Mostramos la nueva ubicación recibida
+        updateUI(location);
+    }
+
+
+
+
+
+
 }
 
